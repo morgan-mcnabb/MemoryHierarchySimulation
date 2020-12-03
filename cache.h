9b1bc64380f7cb6f8cbf7b1bd70b8a507fd69788
wittmana <wittmana@etsu.edu>
2020-12-02 19:01:51 -0700

    Added calculation of Tag Bits and changed cache to except 64bit addresses

--- cache.h
+++ cache.h
@@ -11,7 +11,7 @@ class DataCache
 {
 private:
 
-    unsigned int * cache;
+    uint64_t * cache;
     unsigned int num_entries;
     unsigned int set_size;
     unsigned int line_size;
@@ -21,14 +21,45 @@ private:
     int total_index_bits;
     int total_offset_bits;
 
-    void checkCache(unsigned int physicalAddress)
-    {
+    void checkCache(unsigned int physicalAddress) {
         unsigned int index = physicalAddress;
         unsigned int bitManager = 0xFFFFFFFF;
+
+        // Get index from the physical address
         index = index >> total_offset_bits;                 // Right shift so that the index is the LSB
         bitManager = bitManager >> (32 - total_index_bits); // Right shift so that only the index bits are left
         index = index & bitManager;                         // Gets the index in the cache
         index = index * num_entries;                        // Gets the offset for that cache index
+
+        printf("Index: %x\n", index);
+
+        // Get Tag from the physical address
+        bitManager = 0xFFFFFFFF;
+        unsigned int tag = physicalAddress;                     // Copy physical address to get the tag
+        bitManager = bitManager >> (32 - total_tag_bits);       // Right shift to equal the number of 1 bits = tag bits
+        tag = tag >> (total_offset_bits + total_index_bits);    // Right shift so the tag is the LSB
+        tag = tag & bitManager;                                 // And the bit manager and tag to get the tag bits
+
+        printf("Tag: %x\n", tag);
+
+        uint64_t addAddress = 0x2;
+        addAddress = addAddress << (address_size);
+        addAddress = physicalAddress | addAddress;
+
+        if (cache[2] == NULL)
+            cache[2] = addAddress;
+        printf("%jx", cache[2]);
+
+        // Loop through number of entries for that given index
+        for (int i = 0; i < num_entries; i++)
+        {
+
+        }
+
+
+
+
+
 
     }
 
@@ -47,17 +78,18 @@ public:
         total_offset_bits = log2(line_size);
         total_tag_bits = address_size - total_offset_bits - total_index_bits;
 
-        cache = static_cast<unsigned int *>(malloc((set_size * num_entries) * line_size));
+        cache = static_cast<uint64_t *>(calloc((num_entries * set_size), sizeof(uint64_t)));
+
     }
 
     void read(unsigned int physicalAddress)
     {
-
+        checkCache(physicalAddress);
     }
 
-    void wright(unsigned int physicalAddress)
+    void write(unsigned int physicalAddress)
     {
-
+        checkCache(physicalAddress);
     }
 
     void freeDataCache()
