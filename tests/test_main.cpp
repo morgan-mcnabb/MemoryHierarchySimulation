df6f4033bc1692f22cb730fa745f364f6904df22
Morgan <morgan.mcnabb@protonmail.com>
2020-12-03 12:38:09 -0500

    Spooky scary merge conflicts?

---
+++ tests/test_main.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE example
+#include "../BoostTest/test/included/unit_test.hpp"
+
+using namespace boost::unit_test;
+
+test_suite* init_unit_test_suite(int/* argc*/, char* /*argv*/[])
+{
+    
+    return 0;
+}
