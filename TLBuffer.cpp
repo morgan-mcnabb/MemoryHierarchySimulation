f454403a06eb4ad611004434ef181d957f92ebbc
Morgan <morgan.mcnabb@protonmail.com>
2020-12-02 20:14:45 -0500

    Added comments to the header and implementation files for clarity.

--- TLBuffer.cpp
+++ TLBuffer.cpp
@@ -1,3 +1,12 @@
+/////////////////////////////////////
+//  Author: Morgan McNabb
+//  File: TLBuffer.cpp
+//  Class: CSCI-4727-940 Operating Systems
+//  Due Date: December 9, 2020
+//  Creation Date: December 2, 2020
+//  Last Updated: December 2, 2020
+/////////////////////////////////////
+
 #include "TLBuffer.h"
 
 
@@ -10,16 +19,24 @@ TLBuffer::TLBuffer(int capacity)
 
 bool TLBuffer::lookup(int virtual_page_number)
 {
+    // if the iterator reaches the end, the VPN 
+    // does not exist in the TLB
     if(tlb.find(virtual_page_number) == tlb.end())
         return false;
 
+    // for indexing through the LRU entries inside the TLB
     std::deque<int>::iterator lru_index = lru_entries.begin();
+
+    // do this until we find the VPN in the LRU cache
     while(*lru_index != virtual_page_number)
     {
         lru_index++;
     }
 
+    // remove the VPN at its location in the TLB cache
     lru_entries.erase(lru_index);
+
+    // push the VPN to the front of the TLB cache
     lru_entries.push_front(virtual_page_number);
 
     return true;
@@ -27,12 +44,13 @@ bool TLBuffer::lookup(int virtual_page_number)
 
 void TLBuffer::insert(int virtual_page_number, int physical_frame_number)
 {
-    // m = tlb
-    // tlb = lru_entries
+    // if the VPN is not in the tlb
     if(tlb.find(virtual_page_number) == tlb.end())
     {
+        // if the TLB cache is full
         if(size == lru_entries.size())
         {
+            // remove the least recently used TLB entry
             int last = lru_entries.back();
             lru_entries.pop_back();
             tlb.erase(last);
@@ -40,59 +58,25 @@ void TLBuffer::insert(int virtual_page_number, int physical_frame_number)
     }
     else
     {
+        // for indexing through the LRU entries inside the TLB
         std::deque<int>::iterator lru_index = lru_entries.begin();
+        //
+        // do this until we find the VPN in the LRU cache
         while(*lru_index != virtual_page_number)
         {
             lru_index++;
         }
 
+        // remove the VPN from its current location in both
+        // the TLB cache and the TLB
         lru_entries.erase(lru_index);
         tlb.erase(virtual_page_number);
     }
 
+    // insert the VPN at the most recently used location in the TLB cache
     lru_entries.push_front(virtual_page_number);
+
+    // match the VPN to the PFN in the TLB
     tlb[virtual_page_number] = physical_frame_number;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    if(tlb.find(virtual_page_number) == tlb.end())
-    {
-        if(size == lru_entries.size())
-        {
-            int last = lru_entries.back();
-            lru_entries.pop_back();
-            tlb.erase(last);
-        }
-        else
-        {
-            std::deque<int>::iterator lru_index = lru_entries.begin();
-            while(*lru_index != virtual_page_number)
-            {
-                lru_index++;
-            }
-
-            lru_entries.erase(lru_index);
-            tlb.erase(virtual_page_number);
-        }
-    }
-
-    lru_entries.push_front(virtual_page_number);
-    tlb[virtual_page_number] = physical_frame_number;
-*/
 }
 
