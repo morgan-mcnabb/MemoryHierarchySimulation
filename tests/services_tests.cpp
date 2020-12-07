5cb2d245126a85b5d7c3a14b24e29b57ef99cf2b
Morgan <morgan.mcnabb@protonmail.com>
2020-12-07 17:14:17 -0500

    Expanded services tests to test for opening the file correctly as well as parsing the data correctly.

--- tests/services_tests.cpp
+++ tests/services_tests.cpp
@@ -9,8 +9,10 @@
 #include "boost/test/unit_test.hpp"
 #include "../Services.h"
 
-BOOST_AUTO_TEST_CASE(services_parse_config_test)
+BOOST_AUTO_TEST_CASE(services_parse_config_file_test)
 {
+    auto old_buffer = std::cout.rdbuf();
+    std::cout.rdbuf(nullptr);
     // this will print an error message from parse config, but
     // that is what we are expecting...
     BOOST_CHECK(!parse_config("../bad_trace.config"));
@@ -20,5 +22,29 @@ BOOST_AUTO_TEST_CASE(services_parse_config_test)
     // this will print an error message from parse config, but
     // that is what we are expecting...
     BOOST_TEST(!parse_config("ThisFileDoesNotExist.txt"));
+    std::cout.rdbuf(old_buffer);
+}
+
+BOOST_AUTO_TEST_CASE(services_parse_config_data_test)
+{
+    auto old_buffer = std::cout.rdbuf();
+    std::cout.rdbuf(nullptr);
+
+    BOOST_TEST(parse_config("../trace.config"));
+    BOOST_TEST(TLB_c.num_entries == 2);
+
+    BOOST_TEST(page_table_c.num_virtual_pages == 64);
+    BOOST_TEST(page_table_c.num_physical_pages == 4);
+    BOOST_TEST(page_table_c.page_size == 256);
+
+    BOOST_TEST(cache_c.num_entries == 4);
+    BOOST_TEST(cache_c.set_size == 1);
+    BOOST_TEST(cache_c.line_size == 16);
+    BOOST_TEST(!cache_c.write_through_no_write_allocate);
+    
+    BOOST_TEST(cache_c.virtual_addresses);
+    BOOST_TEST(cache_c.TLB);
+    std::cout.rdbuf(old_buffer);
+
 }
 
